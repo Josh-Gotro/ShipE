2047978f13697d150a3ffe5bdd5e154bb4e35207
Josh-Gotro <joshuagauthreaux@gmail.com>
2021-05-03 23:28:13 -0500

    add Mongo creds

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const app = express();
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
-const MongURI = require('.env/MongoURI')
+// const MongURI = require('.env')
 
-const app = express();
+require('dotenv').config();
 
-mongoose.connect(MongURI);
+
+mongoose.connect(process.env.MongoURI);
 mongoose.connection.once('open', () => {
     console.log('connected to database')
 })
