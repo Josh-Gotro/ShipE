e31bc8c4672fa0abaf5b8ec53b42dcdbf5fa4b98
Josh-Gotro <joshuagauthreaux@gmail.com>
2021-05-05 08:47:08 -0500

    add state hook, include onChange events

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -4,9 +4,9 @@ import { getAuthorsQuery } from '../queries/queries'
 
 const AddBook = () => {
     const { loading, error, data } = useQuery(getAuthorsQuery);
-    const [name, setName] = useState("")
-    const [genre, setGenre] = useState("")
-    const [authorID, setAuthorID] = useState("")
+    const [name, setName] = useState("");
+    const [genre, setGenre] = useState("");
+    const [authorID, setAuthorID] = useState("");
 
 
     const displayAuthors = () => {
@@ -18,23 +18,28 @@ const AddBook = () => {
         ));
     }
 
+    const submitForm = (e) => {
+        e.preventDefault();
+        console.log(name, genre, authorID)
+    }
+
     return (
         <>
-            <form id="add-book">
+            <form id="add-book" onSubmit={e => submitForm(e)}>
 
                 <div className="field">
                     <label>Book name:</label>
-                    <input type="text" />
+                    <input type="text" onChange={(e) => setName(e.target.value)} />
                 </div>
 
                 <div className="field">
                     <label>Genre:</label>
-                    <input type="text" />
+                    <input type="text" onChange={(e) => setGenre(e.target.value)} />
                 </div>
 
                 <div className="field">
                     <label>Author:</label>
-                    <select>
+                    <select onChange={(e) => setAuthorID(e.target.value)}>
                         <option>Select Author</option>
                         {displayAuthors()}
                     </select>
