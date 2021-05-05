26c2bad37dda090642ea46248ed6048acd80e427
Josh-Gotro <joshuagauthreaux@gmail.com>
2021-05-04 22:57:08 -0500

    create react app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import BookList from './components/BookList'
+
+function App() {
+  return (
+    <div id="main" className="App">
+     <h1>Sup DAWGGG</h1>
+     <BookList />
+    </div>
+  );
+}
+
+export default App;
