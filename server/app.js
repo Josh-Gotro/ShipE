b84f903f1b8690a8d74c6837cd175aac71e1ffc7
Josh-Gotro <joshuagauthreaux@gmail.com>
2021-05-06 09:19:48 -0500

    create webhook listener /batch

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
-const {graphqlHTTP} = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const cors = require('cors')
+// const bodyParser = require('body-parser')
+
 
 require('dotenv').config();
 
@@ -16,12 +18,26 @@ mongoose.connection.once('open', () => {
     console.log('connected to database')
 })
 
+app.use('/batch', express.urlencoded({
+    extended: false
+}));
+
+app.use('/batch', express.json());
+
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
 
+app.post("/batch", (req, res) => {
+    console.log(req.body) // Call your action on the request here
+    res.status(200).end() // Respond
+})
+
+
 app.listen(4000, () => {
     console.log("now listening for requests on port 4000")
-})
+}); 
+
+
 
