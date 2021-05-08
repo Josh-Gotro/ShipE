1f1ab1ada7bcfed10466705b92c0e6457f559ddb
Josh-Gotro <joshuagauthreaux@gmail.com>
2021-05-08 07:36:50 -0500

    remove /batch, add /sms, handle post request for /sms

--- server/app.js
+++ server/app.js
@@ -6,6 +6,8 @@ const fs = require('fs');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const cors = require('cors')
+const baseUrl = 'https://api.shipengine.com';
+
 
 // Enable .env 
 require('dotenv').config();
@@ -14,35 +16,39 @@ require('dotenv').config();
 app.use(cors())
 
 // Connect to MongoDB
-mongoose.connect(process.env.MongoURI);
+mongoose.connect(process.env.MongoURI, { useNewUrlParser: true, useUnifiedTopology: true } );
 mongoose.connection.once('open', () => { console.log('connected to database') })
 
 // handle incoming data for each endpoint
 app.use(['/sms', '/batch'],express.urlencoded({ extended: false }));
 
-app.use(['/sms', '/batch'], express.json());
+app.use('/batch', express.json());
+
+app.use('/sms', function (req, res, next) {
+    res.contentType('application/xml');
+    next();
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
 
-// Listen
-app.listen(4000, () => {
-    console.log("now listening for requests on port 4000")
-}); 
-
 // Handle Post Requests
-app.post('/batch', async (req, res) => {
-    const resourceUrl = req.body.resource_url;
-    console.log(req.body, resourceUrl) // Action
+// app.post('/sms', async (req, res) => {
+//     console.log(req.body, "lolSMS") // Action 
+//     res.sendStatus(200); // Respond
+// })
+
+app.all('/sms', function (request, res) {
+    let from_number = request.body.From || request.query.From;
+    let to_number = request.body.To || request.query.To;
+    let text = request.body.Text || request.query.Text;
     res.sendStatus(200); // Respond
-})
+    console.log('Message received - From: ' + from_number + ', To: ' + to_number + ', Text: ' + text);
+});
 
-app.post('/sms', async (req, res) => {
-    console.log(req.body, "lolSMS") // Action 
-    res.sendStatus(200); // Respond
-})
-
-
-
+// Listen
+let server = app.listen(4000, function () {
+    console.log('Listening on port %d', server.address().port);
+});
